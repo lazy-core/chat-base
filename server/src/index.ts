c2b220041cc2940125e3f502aa58dd64aa709477
Emmanuel Penkra <penkra.emmanuel@gmail.com>
2025-04-05 21:06:22 +0000

    traefik & docker

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,13 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
 // Example endpoint
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from the Node.js server with TypeScript!');
+  res.send('Hello from the Node.js Server with TypeScript!');
 });
 
 app.listen(PORT, () => {
